26ae6432e4271f6767f318e591b12b3d96021ff7
Onichaa <91194692+Onichaa@users.noreply.github.com>
2024-03-28 11:48:22 +0800

    Update toimage.go

--- cmd/convert/toimage.go
+++ cmd/convert/toimage.go
@@ -6,12 +6,14 @@ import (
   "os"
   "os/exec"
   "bytes"
+  "regexp"
+  "io/ioutil"
 )
 
 func init() {
   x.NewCmd(&x.ICmd{
-    Name: "(toimage|toimg)",
-    Cmd:  []string{"toimage"},
+    Name: "(toimage|toimg|tovideo)",
+    Cmd:  []string{"toimage", "tovideo"},
     Tags: "convert",
     IsMedia: true,
     Prefix: true,
@@ -20,6 +22,8 @@ func init() {
 
 
       if m.IsQuotedSticker {
+
+        if reg, _ := regexp.MatchString(`(toimage|toimg)`, m.Text); reg {    
         conjp := "./tmp/" + m.ID + ".jpg"
         conwb := "./tmp/" + m.ID + ".webp"
         byte, _ := sock.WA.Download(m.Quoted.QuotedMessage.StickerMessage)
@@ -38,20 +42,58 @@ func init() {
 
         // Check error
         if err != nil {
-          fmt.Println("Error:", err)
+           fmt.Println("Error:", err)
+          //m.Reply("Jangan reply stiker yang bergerak, harus nya ketik .tovideo untuk stiker yang bergerak menjadi video")
           return
         }
-        
+
         url, err := x.Upload(conjp)
         if err != nil {
             fmt.Println("Error:", err)
             return
         }
-       
+
         sock.SendImage(m.From, url, "", *m)
-        
+
         os.Remove(conwb)
         os.Remove(conjp)
+
+        } else {
+        conwb := "./tmp/" + m.ID + ".webp"
+        outMp4 := "./tmp/" + m.ID + ".mp4"
+        outGif := "./tmp/" + m.ID + ".gif"
+        byte, _ := sock.WA.Download(m.Quoted.QuotedMessage.StickerMessage)
+        err := ioutil.WriteFile(conwb, byte, 0644)
+        if err != nil {
+          fmt.Println("Failed saved webp")
+          return
+        }
+       
+        cmd := exec.Command("convert", conwb, outGif)
+         err = cmd.Run(); 
+        if err != nil {
+            fmt.Println("Error:", err)
+            return
+        }
+
+        cmd = exec.Command("ffmpeg", "-i", outGif, "-vf", "crop=trunc(iw/2)*2:trunc(ih/2)*2", "-b:v", "0", "-crf", "25", "-f", "mp4", "-vcodec", "libx264", "-pix_fmt", "yuv420p", outMp4)
+         err = cmd.Run(); 
+          if err != nil {
+            fmt.Println("Error:", err)
+            return
+        }
+
+
+          url, err := x.Upload(outMp4)
+          if err != nil {
+              fmt.Println("Error:", err)
+              return
+          }
+          sock.SendVideo(m.From, url, "Converted sticker to video", *m)
+          os.Remove(outGif)
+          os.Remove(outMp4)
+          os.Remove(conwb)
+        }
         m.React("✅")
       }
     },
